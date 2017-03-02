53cbc51583898a65491d7980bc7031712299a6fd
Xiao QingSong <net19880504@126.com>
2017-03-02 21:32:28 +0800

    First commit

---
+++ demo/src/main/java/com/chanson/library/sample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.chanson.library.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Administrator on 2017/3/2 0002.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.main);
+    }
+}
